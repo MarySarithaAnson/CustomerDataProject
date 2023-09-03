659aa982b8b8a8a968d6b048efe5b05c5d964e0c
MarySarithaAnson <143943550+MarySarithaAnson@users.noreply.github.com>
2023-09-03 20:26:05 +0530

    Update CustomerInfo.cs

--- CustomerDataProject/Models/CustomerInfo.cs
+++ CustomerDataProject/Models/CustomerInfo.cs
@@ -1,7 +1,10 @@
-﻿namespace CustomerDataProject.Models
+using System.ComponentModel.DataAnnotations;
+namespace CustomerDataProject.Models
 {
     public class CustomerInfo
     {
+        [Key]
+        public int CustomerId { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public int Phone { get; set; }
